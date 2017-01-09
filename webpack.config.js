163d73395b120018de114417044fd67dbd27406c
Bengi <bengitiger@gmail.com>
2017-01-10 07:34:18 +0900

    babel-es2015

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,19 @@ module.exports = {
         output: {
                 path: 'dist',
                 filename: 'bundle.js'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015']
+                    }
+                }
+            ]
         }
 
 };
