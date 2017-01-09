69ab6afbe561a1e584087ab7122fa5bff92c2301
Bengi <bengitiger@gmail.com>
2017-01-10 07:19:31 +0900

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+        // define entry point
+        entry: './src/script-1.js',
+
+        // define ouput point
+        output: {
+                path: 'dist',
+                filename: 'bundle.js'
+        }
+
+};
